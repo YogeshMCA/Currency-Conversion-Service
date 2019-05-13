5b6121192146351f54d70eedc0ca28fea2b1f9d4
yogesh <yogee1389@gmail.com>
2019-05-13 15:03:17 +0530

    Service Registry

--- src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
+++ src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
@@ -1,17 +1,16 @@
 package com.example.microservice.CurrencyConversionService.service;
 
-import java.math.BigDecimal;
 
-
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.microservice.CurrencyConversionService.Bean.CurrencyConversionBean;
 
 //@FeignClient(name="exchange-service",url= "${feign.client.url}")
-@FeignClient("https://currexchange")
-//@RibbonClient("CurrExchange") //Added multiple service with the exchange-service name in application.properties file for load balancing
+@FeignClient("currexchange")
+@RibbonClient("currexchange") //Added multiple service with the exchange-service name in application.properties file for load balancing
 public interface CurrencyExchangeProxy {
 	@GetMapping("/exchange-service/from/{from}/to/{to}")
 	public CurrencyConversionBean getExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to);
