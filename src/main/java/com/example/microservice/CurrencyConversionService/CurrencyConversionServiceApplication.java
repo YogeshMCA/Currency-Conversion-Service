c687e9a4e34211f09754733c38134821ceda1c7a
yogesh <yogee1389@gmail.com>
2019-07-20 21:40:18 +0530

    Final Version

--- src/main/java/com/example/microservice/CurrencyConversionService/CurrencyConversionServiceApplication.java
+++ src/main/java/com/example/microservice/CurrencyConversionService/CurrencyConversionServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableFeignClients("com.example.microservice.CurrencyConversionService")
 public class CurrencyConversionServiceApplication {
--- src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
+++ src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.example.microservice.CurrencyConversionService.Bean.CurrencyConversionBean;
 
 //@FeignClient(name="exchange-service",url= "${feign.client.url}")
-@FeignClient("currexchange")
-@RibbonClient("currexchange") //Added multiple service with the exchange-service name in application.properties file for load balancing
+@FeignClient("curr-exchange")
+@RibbonClient("curr-exchange") //Added multiple service with the exchange-service name in application.properties file for load balancing
 public interface CurrencyExchangeProxy {
 	@GetMapping("/exchange-service/from/{from}/to/{to}")
 	public CurrencyConversionBean getExchangeValue(@PathVariable("from") String from,@PathVariable("to") String to);
