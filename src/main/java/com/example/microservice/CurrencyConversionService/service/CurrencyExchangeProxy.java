fe861e169704fc98604ea9eebd4b322c3a30a23c
yogesh <yogee1389@gmail.com>
2019-07-25 11:14:05 +0530

    Notes

--- src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
+++ src/main/java/com/example/microservice/CurrencyConversionService/service/CurrencyExchangeProxy.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.microservice.CurrencyConversionService.Bean.CurrencyConversionBean;
 
+/*
+ * Spring Cloud integrates Ribbon and Eureka to provide a load balanced http client when using Feign.
+ * In the @FeignClient annotation the String value ("curr-exchange" above) is an arbitrary client name, which is used to create a Ribbon load balancer
+ * The Ribbon client above will want to discover the physical addresses for the "curr-exchange" service. If your application is a Eureka client then it will resolve the service in the Eureka service registry. 
+ * If you don’t want to use Eureka, you can simply configure a list of servers
+ */
 //@FeignClient(name="exchange-service",url= "${feign.client.url}")
 @FeignClient("curr-exchange")
 @RibbonClient("curr-exchange") //Added multiple service with the exchange-service name in application.properties file for load balancing
