1c2e6ab506710a9b578f7e3484636923465a6a79
yogesh <yogee1389@gmail.com>
2019-08-23 10:49:42 +0530

    CrossOrigin Resource Sharing

--- src/main/java/com/example/microservice/CurrencyConversionService/Controller/CurrencyConversionController.java
+++ src/main/java/com/example/microservice/CurrencyConversionService/Controller/CurrencyConversionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 
 @RestController
+@CrossOrigin(allowedHeaders="*",origins="*")
 public class CurrencyConversionController {
 
 	@Autowired
